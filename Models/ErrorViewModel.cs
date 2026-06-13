80a706d1be2544aba741d0dfc471b35378e6fe1b
Ripichip <105686182+Reapson@users.noreply.github.com>
2026-06-13 15:22:16 -0600

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Practica_Evaluada_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Practica_Evaluada_1
+Practica Evaluada #1 Curso de Programacion Avanzada Web
